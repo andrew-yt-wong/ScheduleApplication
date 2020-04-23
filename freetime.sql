d0f921659a60886bd32f43250677f78d0ef0e210
andrew-yt-wong <andrew-yt-wong@gmail.com>
2020-04-23 15:16:01 -0700

    Added Calendar.html

--- freetime.sql
+++ freetime.sql
@@ -3,19 +3,13 @@ CREATE DATABASE FreeTime;
 
 USE FreeTime;
 
-CREATE TABLE Friends (
-	friend_id INT PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(45) NOT NULL,
-    email VARCHAR(45) NOT NULL,
-    schedule_id VARCHAR(45) NOT NULL
-);
-
 CREATE TABLE Events (
 	event_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(45) NOT NULL,
-    start_time TIME,
-    end_time TIME,
-    day VARCHAR(45) NOT NULL
+    start_time TIME NOT NULL,
+    duration DOUBLE NOT NULL,
+    day VARCHAR(45) NOT NULL,
+    color VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE Schedules (
@@ -27,11 +21,24 @@ CREATE TABLE Schedules (
 CREATE TABLE Users (
 	user_id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(45) NOT NULL,
-    password VARCHAR(45) NOT NULL
+    password VARCHAR(256) NOT NULL
     -- friend_id INT, 
     -- schedule_id INT,
     -- FOREIGN KEY fk1(friend_id) REFERENCES Friends(friend_id),
     -- FOREIGN KEY fk2(schedule_id) REFERENCES Schedules(schedule_id)
 );
 
-INSERT INTO Users (username, password) VALUES ('awong827', 'password');
+CREATE TABLE Friends (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    friend_id INT,
+    FOREIGN KEY user(user_id) REFERENCES Users(user_id),
+    FOREIGN KEY friend(friend_id) REFERENCES Users(user_id)
+);
+
+INSERT INTO Users (username, password) VALUES ('user1', 'password');
+INSERT INTO Users (username, password) VALUES ('user2', 'password');
+
+INSERT INTO Friends (user_id, friend_id) VALUES (1, 2);
+
+
